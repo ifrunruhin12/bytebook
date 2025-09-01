00e2cd3a73a0af25ad5268a90ee71d8887d5cadc
Ifran Ruhin <ifrunruhin@gmail.com>
2025-09-01 21:14:14 +0600

    Add a sample rust program

---
+++ rust/hello_cargo/target/debug/deps/hello_cargo-eeffcc7c557c9ff2.d
@@ -0,0 +1,5 @@
+/home/pop/Documents/bytebook/rust/hello_cargo/target/debug/deps/hello_cargo-eeffcc7c557c9ff2: src/main.rs
+
+/home/pop/Documents/bytebook/rust/hello_cargo/target/debug/deps/hello_cargo-eeffcc7c557c9ff2.d: src/main.rs
+
+src/main.rs:
